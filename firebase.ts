1989711d9d460777afa4af42998059f5523f2585
NomuraKohei <seiryu2nd@gmail.com>
2023-10-14 16:26:26 +0900

    fix

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAS_gZkizv_hSLyMqEgzGou_TGaOm1dtOw",
-  authDomain: "chatapplication-with-cha-ed507.firebaseapp.com",
-  projectId: "chatapplication-with-cha-ed507",
-  storageBucket: "chatapplication-with-cha-ed507.appspot.com",
-  messagingSenderId: "1068892901558",
-  appId: "1:1068892901558:web:9c8339151abd6a9cb39617"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
